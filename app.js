f3c0de2e87dfd1b6da2ffd8f2bee5a800bb2fefd
LauraKirby <laurakirby26@gmail.com>
2015-06-03 23:04:50 -0700

    add basic calculator functionality

---
+++ app.js
@@ -0,0 +1,44 @@
+//bring in the express module
+var express = require("express"); 
+
+//call the express function, return an object
+var app = express(); //the result of calling our express function 
+
+//get route 
+//where my application starts when I start the server 
+
+app.set("view engine", "ejs");  //mkdir views 
+
+//"/" means //localhost:3000/ 
+
+app.get("/", function (req, res){
+	var name = "Elie"; 
+	res.render("index", {personName:name}); //this will render a view and not just just send back text (which is what send did)
+});
+
+app.get('/person/:name', function(req, res){ //:name means something, you can put anything here
+	var person = req.params.name; //this will refer to the url params, this "name" refers to ":name"
+	console.log(req.params.name);
+	res.render("person", {specificPerson:person}); //this third person refers the 
+})
+
+app.get("/add/:num1/:num2", function (req, res){
+	var sum = req.params.num1 + req.params.num2; 
+	res.render("math", {number:sum}); //this will render a view and not just just send back text (which is what send did)
+});
+
+app.get("/sub/:num1/:num2", function (req, res){
+	var subtract = req.params.num1 - req.params.num2; 
+	res.render("math", {number:subtract}); //this will render a view and not just just send back text (which is what send did)
+});
+
+app.get("/div/:num1/:num2", function (req, res){
+	var divide = (req.params.num1)/(req.params.num2); 
+	res.render("math", {number:divide}); //this will render a view and not just just send back text (which is what send did)
+});
+
+app.listen(3000,function(){
+	console.log("server starting on port 3000");
+})
+
+//npm install -g nodemon
